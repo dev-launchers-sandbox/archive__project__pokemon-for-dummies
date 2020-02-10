0a27ed34d0e62bdd284cb999c1e991eda00d2024
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-10 21:54:14 +0000

    Deploy dev-launchers-sandbox/project__pokemon-for-dummies to github.com/dev-launchers-sandbox/project__pokemon-for-dummies.git:gh-pages

---
+++ precache-manifest.0ff49dcd2c340b5aee1ba04627125c15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950ac76c170c1141e76a5c3a161c94a6",
+    "url": "/dev/project__pokemon-for-dummies/index.html"
+  },
+  {
+    "revision": "3edeebba536f7d193f3f",
+    "url": "/dev/project__pokemon-for-dummies/static/css/main.a31bcc75.chunk.css"
+  },
+  {
+    "revision": "11875e3af915caea0a04",
+    "url": "/dev/project__pokemon-for-dummies/static/js/2.358d5879.chunk.js"
+  },
+  {
+    "revision": "3edeebba536f7d193f3f",
+    "url": "/dev/project__pokemon-for-dummies/static/js/main.04d56e43.chunk.js"
+  },
+  {
+    "revision": "8cd8159d005291216a14",
+    "url": "/dev/project__pokemon-for-dummies/static/js/runtime~main.6a237561.js"
+  }
+]);
